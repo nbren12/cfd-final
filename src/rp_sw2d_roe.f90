690dfd5d79170c069e51f36ab8454850e04a3196
Noah D. Brenowitz <nbren12@gmail.com>
2013-05-11 01:40:55 -0400

    Finished calc_chars subroutine for solving the riemmann problem on all grid cells

--- src/rp_sw2d_roe.f90
+++ src/rp_sw2d_roe.f90
@@ -1,10 +1,11 @@
 module rp_sw2d_roe
+
 contains
 
 subroutine calc_update(L,S,R,ql,qr,n,g,dt,dx,F)
 implicit none
-real(8) L(2,2,n),R(2,2,n),S(2,n),alpha(2,n),F(2,n-1)
-real(8), dimension(2,n) :: ql ,qr
+real(8) L(3,3,n),R(3,3,n),S(3,n),alpha(3,n),F(3,n-1)
+real(8), dimension(3,n) :: ql ,qr
 real(8) :: dt,dx,g
 integer n,i
 intent(out) F
@@ -22,49 +23,84 @@ end do
 
 end subroutine calc_update
 
-subroutine calc_chars(L,S,R,ql,qr,n,g,dt,dx)
+subroutine calc_chars(ax,ay,sx,sy,rx,ry,q,nx,ny,g,dt,dx)
 implicit none
-real(8) L(2,2,n),R(2,2,n),S(2,n)
-real(8), dimension(2,n) :: ql ,qr
-real(8) :: dt,dx,g
-integer n,i
-intent(out) L,S,R 
 
+real(8),dimension(3,nx-1,ny-1) :: ax,ay,sx,sy 
+real(8),dimension(3,3,nx-1,ny-1) :: rx,ry
+real(8) q(3,nx,ny),g,dt,dx
+integer nx,ny,i,j
+intent(out) ax,ay,sx,sy,rx,ry
 
-! Calculate the fluctuations for each grid cell
-do i=1,n
-
-    ! Calculate the Fluctations
-    call roe_solve(L(:,:,i),S(:,i),R(:,:,i),ql(:,i),qr(:,i),g)
+do i = 1, nx-1
+    do j = 1, ny-1
+        call roe_solve_x(ax(:,i,j),sx(:,i,j),rx(:,:,i,j),q(:,i,j),q(:,i+1,j),g)
+        call roe_solve_y(ay(:,i,j),sy(:,i,j),ry(:,:,i,j),q(:,i,j),q(:,i,j+1),g)
+    end do
 end do
+
 end subroutine calc_chars
 
-subroutine roe_solve(L,S,R,ql,qr,g)
+subroutine roe_solve_y(alpha,S,R,ql,qr,g)
 implicit none
-!double precision flux(2),ql(2),qr(2),g
-real(8), dimension(3,2) :: R,L
+real(8), dimension(3,3) :: R,R_p
+real(8),dimension(3) :: ql,ql_p,qr,qr_p,&
+    alpha,alpha_p,S,S_p
+
+real(8) g
+integer i,p(3)
+intent(out) alpha,R,S
+
+p = (/1,3,2/)
+
+do i=1,3
+ql_p(i) = ql(p(i))
+qr_p(i) = qr(p(i))
+end do
+
+call roe_solve_x(alpha_p,S_p,R_p,ql_p,qr_p,g)
+
+do i=1,3
+    R(p(i),:) = R_p(i,:)
+    alpha(p(i)) = alpha_p(i)
+    S(p(i)) = S_p(i)
+end do
+
+end subroutine roe_solve_y
+
+subroutine roe_solve_x(alpha,S,R,ql,qr,g)
+implicit none
+real(8), dimension(3,3) :: R,L
 real(8),dimension(3) :: ql,qr,alpha,S
 real(8) g,u_hat,v_hat,h_bar,c_hat
 
 integer i
-intent(out) L,R,S
+intent(out) alpha,R,S
 
 u_hat =( sqrt(ql(1))*ql(2)/ql(1) + sqrt(qr(1))*qr(2)/qr(1))&
         /(sqrt(qr(1))+sqrt(ql(1)))
+v_hat =( sqrt(ql(1))*ql(3)/ql(1) + sqrt(qr(1))*qr(3)/qr(1))&
+        /(sqrt(qr(1))+sqrt(ql(1)))
+
 h_bar = (ql(1)+qr(1))/2.0
+
 c_hat = sqrt(g*h_bar)
 
-R(1,:) = (/1.0, 1.0 /)
-R(2,:) = (/u_hat-c_hat,u_hat+c_hat/)
+R(:,1) = (/0.0D0,0.0D0,1.0d0/)
+R(:,2) = (/1.0D0,u_hat+c_hat,v_hat/)
+R(:,3) = (/1.0D0,u_hat-c_hat,v_hat/)
 
-L(1,:) = (/u_hat+c_hat,-1.0D0/)
-L(2,:) = (/-u_hat+c_hat,1.0D0/)
-L = L/ 2.0 / c_hat
 
-S(1) = u_hat - c_hat
-S(2) = u_hat + c_hat
+L(1,:) = (/-v_hat,0.0d0,1.0d0/)
+L(2,:) = (/u_hat+c_hat,-1.0D0,0.0d0/)/2.0d0/c_hat
+L(3,:) = (/-u_hat+c_hat,1.0D0,0.0d0/)/2.0d0/c_hat
 
-end subroutine roe_solve  
+S = (/u_hat,u_hat+c_hat,u_hat-c_hat/)
+
+
+alpha = matmul(L,qr-ql)
+
+end subroutine roe_solve_x  
 
 function PHI (theta) result(y)
 implicit none
@@ -74,4 +110,4 @@ intent(in) theta
 tmp = max(min(1.0D0,2*theta),min(2.0D0,theta))
 y = max(0.0D0, tmp)
 end function PHI
-end module rp_sw1d_roe
+end module rp_sw2d_roe
